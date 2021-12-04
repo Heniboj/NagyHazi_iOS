69fd016e7d86e175be2e847ac8b7cd22f433bd61
heniboj <berenyihenrik2001@gmail.com>
2021-12-04 16:28:01 +0100

    One way toggle

---
+++ iFly/iFly/Model/MyToggleStyle.swift
@@ -0,0 +1,36 @@
+//
+//  MyToggleStyle.swift
+//  iFly
+//
+//  Created by Henrik Berényi on 2021. 12. 03..
+//
+
+import Foundation
+import SwiftUI
+
+struct MyToggleStyle: ToggleStyle {
+    var onColor = Color(UIColor.purple)
+    var offColor = Color(UIColor.purple)
+    var thumbColor = Color.white
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        HStack {
+            configuration.label // The text (or view) portion of the Toggle
+            Spacer()
+            RoundedRectangle(cornerRadius: 16, style: .circular)
+                .fill(configuration.isOn ? onColor : offColor)
+                .frame(width: 50, height: 29)
+                .overlay(
+                    Circle()
+                        .fill(thumbColor)
+                        .shadow(radius: 1, x: 0, y: 1)
+                        .padding(1.5)
+                        .offset(x: configuration.isOn ? 10 : -10))
+                .onTapGesture { configuration.isOn.toggle() }
+                .animation(Animation.easeInOut(duration: 0.2), value: configuration.isOn)
+                
+        }
+        .font(.title)
+        .padding(.horizontal)
+    }
+}
--- iFly/iFly/View/SearchView.swift
+++ iFly/iFly/View/SearchView.swift
@@ -15,6 +15,9 @@ struct SearchView: View {
     @ObservedObject var searchHandler:SearchHandler// = SearchHandler()
     @State private var isActive = false
     
+    @State private var isOneWay = false
+    @State private var datepadding = 12
+    
     var body: some View {
         VStack {
             VStack {
@@ -22,14 +25,28 @@ struct SearchView: View {
                 TextField("Going to", text:$searchHandler.goingText).textFieldStyle(.roundedBorder).font(Font.system(size: 25, design: .default))
                 VStack {
                     HStack {
-                        Text("Departure").frame(maxWidth: .infinity, alignment: .leading).padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 0))
-                        Text("Return").frame(maxWidth: .infinity, alignment: .leading).padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 0))
-                }
+                        Text("Departure").frame(maxWidth: .infinity, alignment: .leading).padding(EdgeInsets(top: 0, leading: CGFloat(datepadding), bottom: 0, trailing: 0))
+                        if !isOneWay {
+                            Text("Return").frame(maxWidth: .infinity, alignment: .leading).padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 0))
+                        }
+                    }
                     HStack {
-                        DatePicker("", selection: $searchHandler.departureDate,displayedComponents: [.date]).labelsHidden().frame(maxWidth: .infinity, alignment: .leading).accentColor(Color.purple)
-                    
-                        DatePicker("", selection: $searchHandler.returnDate,displayedComponents: [.date]).labelsHidden().frame(maxWidth: .infinity, alignment: .leading).accentColor(Color.purple)
+                        DatePicker("", selection: $searchHandler.departureDate,displayedComponents: [.date]).labelsHidden().frame(maxWidth: .infinity, alignment: .leading).accentColor(Color.purple).padding(EdgeInsets(top: 0, leading: CGFloat(datepadding)-12, bottom: 0, trailing: 0))
+                        if !isOneWay {
+                            DatePicker("", selection: $searchHandler.returnDate,displayedComponents: [.date]).labelsHidden().frame(maxWidth: .infinity, alignment: .leading).accentColor(Color.purple)
+                        }
                     }.padding(EdgeInsets(top: -20, leading: 0, bottom: 0, trailing: 0))
+                    
+                    HStack {
+                        Toggle("One way", isOn: $isOneWay).toggleStyle(MyToggleStyle()).padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 10))//.onChange(of: isOneWay) { newValue in
+                            //if newValue {
+                           //     datepadding = 100
+                           // } else {
+                          //      datepadding = 12
+                            //}
+                            
+                        //}
+                    }
                 }.padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
             
             }.padding(EdgeInsets(top: 50, leading: 50, bottom: 0, trailing: 50)).preferredColorScheme(.light)
