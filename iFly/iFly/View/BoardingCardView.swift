62c0d9234023a7716b93d0c48cc3a742bb812c66
heniboj <berenyihenrik2001@gmail.com>
2021-12-05 20:55:30 +0100

    BoardingCard styling

--- iFly/iFly/View/BoardingCardView.swift
+++ iFly/iFly/View/BoardingCardView.swift
@@ -13,45 +13,184 @@ struct BoardingCardView: View {
         sortDescriptors: []
     ) var boardingcards: FetchedResults<BoardingCard>
     
-    private let dateFormatter = DateFormatter()
+    private let dayMonth = DateFormatter()
+    private let hourMinute = DateFormatter()
     init() {
-        dateFormatter.dateFormat = "YY/MM/dd hh:mm"
+        dayMonth.dateFormat = "dd MMM"
+        hourMinute.dateFormat = "hh:mm"
     }
     
     var body: some View {
         VStack {
             TabView {
                 ForEach(boardingcards, id: \.self) { bc in
-                    VStack {
-                        
-                        HStack {
-                            VStack {
-                                Text(bc.flight!.startingAirport!.code!)
-                                Text(bc.flight!.startingAirport!.name!)
+                    ScrollView {
+                        VStack {
+                            Group {
+                                Image("Barcode")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding(EdgeInsets(top: 20, leading: 50, bottom: 15, trailing: 50))
+                                
+                                DottedLine()
+                                    .stroke(style: StrokeStyle(lineWidth: 3, dash: [5]))
+                                    .frame(height: 1)
+                                    .foregroundColor(Color.gray)
                             }
-                            Spacer()
-                            VStack {
-                                Text(bc.flight!.destinationAirport!.code!)
-                                Text(bc.flight!.destinationAirport!.name!)
+                            
+                            Group {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(bc.flight!.startingAirport!.code!)
+                                            .font(.largeTitle)
+                                        Text(bc.flight!.startingAirport!.name!)
+                                    }
+                                    .foregroundColor(Color(red: 0.0, green: 0.0, blue: 1.0))
+                                    
+                                    Spacer()
+                                    
+                                    VStack {
+                                        Text(bc.id!) // TODO: should be flight id
+                                        Image(systemName: "airplane")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 25, height: 25)
+                                            .foregroundColor(Color.gray)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    VStack(alignment: .trailing) {
+                                        Text(bc.flight!.destinationAirport!.code!)
+                                            .font(.largeTitle)
+                                        Text(bc.flight!.destinationAirport!.name!)
+                                    }
+                                    .foregroundColor(Color(red: 0.0, green: 0.0, blue: 1.0))
+
+                                }
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
                             }
-                        }
-                        
-                        //Text(bc.id!)
-                        
-                        HStack {
-                            Text(bc.firstName!)
-                            Text(bc.lastName!)
-                        }
-                        
-                        HStack {
-                            Text(dateFormatter.string(from: bc.flight!.departureDate!))
+                            
+                            DottedLine()
+                                .stroke(style: StrokeStyle(lineWidth: 3, dash: [5]))
+                                .frame(height: 1)
+                                .foregroundColor(Color.gray)
+                            
+                            Group {
+                                HStack {
+                                    Text(bc.firstName!)
+                                    Text(bc.lastName!)
+                                    Spacer()
+                                }
+                                .padding(EdgeInsets(top: 15, leading: 25, bottom: 0, trailing: 0))
+                                    .font(.system(size: 20))
+                                
+                                HStack {
+                                    Text("Name")
+                                        .padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 0))
+                                        .font(.system(size: 16))
+                                        .foregroundColor(Color.gray)
+                                    Spacer()
+                                }
+                            }
+                            
+                            
+                            Group {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text("Date")
+                                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 1, trailing: 0))
+                                        Text(dayMonth.string(from: bc.flight!.departureDate!))
+                                            .font(.system(size: 23))
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text("Departure")
+                                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 1, trailing: 0))
+                                        Text(hourMinute.string(from: bc.flight!.departureDate!))
+                                            .font(.system(size: 23))
+                                    }
+                                    
+                                }
+                                .padding(EdgeInsets(top: 10, leading: 25, bottom: 10, trailing: 25))
+                                    .background(Color(red: 0.0, green: 0.0, blue: 1.0))
+                                    .foregroundColor(Color.white)
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 100, alignment: .center)
+                            }
+                            
+                            Group {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text("PNR")
+                                            .foregroundColor(Color.gray)
+                                        Text("EFQQRD") // TODO: should be boarding card id
+                                    }
+                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 40))
+                                    VStack(alignment: .leading) {
+                                        Text("Sequence")
+                                            .foregroundColor(Color.gray)
+                                        Text("0040")
+                                    }
+                                    Spacer()
+                                }
+                                .padding(EdgeInsets(top: 10, leading: 25, bottom: 15, trailing: 25))
+                            }
+                            
+                            DottedLine()
+                                .stroke(style: StrokeStyle(lineWidth: 3, dash: [5]))
+                                .frame(height: 1)
+                                .foregroundColor(Color.gray)
+                            
+                            Group {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Group {
+                                            Text("Passport")
+                                            Text("Issued by")
+                                            Text("Gender")
+                                            Text("Birthdate")
+                                        }
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 1, trailing: 0))
+                                        .foregroundColor(Color.gray)
+                                    }
+                                    .padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 35))
+                                    
+                                    VStack(alignment: .leading) {
+                                        Group {
+                                            Text("BR3557692")
+                                            Text("Hungary")
+                                            Text("MALE")
+                                            Text("08-11-2001")
+                                        }
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 1, trailing: 0))
+                                    }
+
+                                    Spacer()
+                                }
+                                .padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
+                            }
+                            
                             Spacer()
-                            Text(dateFormatter.string(from: bc.flight!.arriveDate!))
                         }
+                        
                     }
                 }
-            }.tabViewStyle(.page).indexViewStyle(.page(backgroundDisplayMode: .always))
+            }
+            .tabViewStyle(.page)
+            .indexViewStyle(.page(backgroundDisplayMode: .always))
         }
+    }
+}
+
+struct DottedLine: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        return path
     }
 }
 
