d6e1097cb32912625c335c84dea41180dfcd396c
heniboj <berenyihenrik2001@gmail.com>
2021-12-09 02:05:09 +0100

    SearchView keyboard fix

--- iFly/iFly/View/SearchView.swift
+++ iFly/iFly/View/SearchView.swift
@@ -16,98 +16,98 @@ struct SearchView: View {
     @State private var isActive = false
     
     @State private var isOneWay = false
-    @State private var datepadding = 12
     
     var body: some View {
-        ZStack {
-            Image("search_background")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
+        VStack {
             VStack {
-                VStack {
-                    Group {
-                        TextField("Leaving from", text:$searchHandler.leavingText)
-                        TextField("Going to", text:$searchHandler.goingText)
-                            .onAppear {
-                                searchHandler.goingText = ""
-                            }
-                    }
-                    .textFieldStyle(.roundedBorder)
-                    .font(Font.system(size: 25, design: .default))
+                Group {
+                    TextField("Leaving from", text:$searchHandler.leavingText)
+                    TextField("Going to", text:$searchHandler.goingText)
+                        .onAppear {
+                            searchHandler.goingText = ""
+                        }
+                }
+                .textFieldStyle(.roundedBorder)
+                .font(Font.system(size: 25, design: .default))
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                        .fill(Color.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
                     
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 15, style: .continuous)
-                            .fill(Color.white)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 50)
-                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
-                        
-                        VStack {
-                            HStack {
-                                Text("Departure")
+                    VStack {
+                        HStack {
+                            Text("Departure")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 0))
+                                .foregroundColor(Color.gray)
+                            if !isOneWay {
+                                Text("Return")
                                     .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(EdgeInsets(top: 0, leading: CGFloat(datepadding), bottom: 0, trailing: 0))
+                                    .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 0))
                                     .foregroundColor(Color.gray)
-                                if !isOneWay {
-                                    Text("Return")
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 0))
-                                        .foregroundColor(Color.gray)
-                                }
                             }
-                            HStack {
-                                DatePicker("", selection: $searchHandler.departureDate,displayedComponents: [.date])
+                        }
+                        HStack {
+                            DatePicker("", selection: $searchHandler.departureDate,displayedComponents: [.date])
+                                .labelsHidden()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .accentColor(Color.purple)
+
+                            if !isOneWay {
+                                DatePicker("", selection: $searchHandler.returnDate,displayedComponents: [.date])
                                     .labelsHidden()
                                     .frame(maxWidth: .infinity, alignment: .leading)
                                     .accentColor(Color.purple)
-
-                                if !isOneWay {
-                                    DatePicker("", selection: $searchHandler.returnDate,displayedComponents: [.date])
-                                        .labelsHidden()
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .accentColor(Color.purple)
-                                }
-                            }.padding(EdgeInsets(top: -20, leading: 0, bottom: 0, trailing: 0))
-                        }.padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
-                    }
-                         
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 15, style: .continuous)
-                            .fill(Color.white)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 50)
-                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
-                        
-                        HStack {
-                            Toggle("One way", isOn: $isOneWay)
-                                .foregroundColor(Color.gray)
-                                .toggleStyle(MyToggleStyle())
-                                .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 10))
-                        }
-                    }
-                        
-                    
-                
-                }.padding(EdgeInsets(top: 50, leading: 50, bottom: 0, trailing: 50))
-                
-                Button(action: {isActive = true}) {
-                    Label("Search Flights", systemImage: "magnifyingglass")
+                            }
+                        }.padding(EdgeInsets(top: -20, leading: 0, bottom: 0, trailing: 0))
+                    }.padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
                 }
-                .padding()
-                .background(Color.purple)
-                .foregroundColor(Color.white)
-                .clipShape(Capsule())
-                
-                
-                NavigationLink(destination: SearchResultView(rootIsActive: self.$rootIsActive, searchHandler:searchHandler, isOneWay:isOneWay), isActive: $isActive) {
+                     
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                        .fill(Color.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
                     
-                }.isDetailLink(false)
+                    HStack {
+                        Toggle("One way", isOn: $isOneWay)
+                            .foregroundColor(Color.gray)
+                            .toggleStyle(MyToggleStyle())
+                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 10))
+                    }
+                }
+                    
                 
-                Spacer()
-            }//.background(Color.purple)//.navigationTitle("Search").navigationBarTitleDisplayMode(.inline)
+            
+            }.padding(EdgeInsets(top: 50, leading: 50, bottom: 0, trailing: 50))
+            
+            Button(action: {isActive = true}) {
+                Label("Search Flights", systemImage: "magnifyingglass")
+            }
+            .padding()
+            .background(Color.purple)
+            .foregroundColor(Color.white)
+            .clipShape(Capsule())
+            
+            
+            NavigationLink(destination: SearchResultView(rootIsActive: self.$rootIsActive, searchHandler:searchHandler, isOneWay:isOneWay), isActive: $isActive) {
+                
+            }.isDetailLink(false)
+            
+            Spacer()
         }
-
+        .background(
+            Image("search_background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+        )
+        .ignoresSafeArea(.keyboard)
+        //.background(Color.purple)//.navigationTitle("Search").navigationBarTitleDisplayMode(.inline)
         
     }
 }
